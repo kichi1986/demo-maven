aaae4aa1b3c0e919261a6716102afe355fce9ab2
kichi1986 <kichi.wonders@gmail.com>
2024-07-28 19:12:25 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello my Dear maven--!!! I WANTED TO LEARN....!!!!" );
+        System.out.println( "Hello my Dear maven--!!! I WANTED TO LEARN and it looks great....!!!!" );
     }
 }
